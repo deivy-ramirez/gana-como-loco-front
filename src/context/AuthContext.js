a7c96f640213d66443dd730c7045f9488c161c82
deivy-ramirez <davydwindsor@gmail.com>
2024-10-30 01:50:12 -0500

    volviendo a la verion funcional3.0

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -10,10 +10,6 @@ export const AuthProvider = ({ children }) => {
     return storedData ? JSON.parse(storedData) : null;
   });
 
-  const [isAdmin, setIsAdmin] = useState(() => {
-    return !!localStorage.getItem('adminKey');
-  });
-
   useEffect(() => {
     if (authData) {
       localStorage.setItem('authData', JSON.stringify(authData));
@@ -24,23 +20,8 @@ export const AuthProvider = ({ children }) => {
 
   const isAuthenticated = !!authData;
 
-  const login = (data) => {
-    setAuthData(data);
-  };
-
-  const logout = () => {
-    setAuthData(null);
-    setIsAdmin(false);
-    localStorage.removeItem('adminKey');
-  };
-
-  const adminLogin = (adminKey) => {
-    localStorage.setItem('adminKey', adminKey);
-    setIsAdmin(true);
-  };
-
   return (
-    <AuthContext.Provider value={{ authData, isAuthenticated, isAdmin, login, logout, adminLogin }}>
+    <AuthContext.Provider value={{ authData, setAuthData, isAuthenticated }}>
       {children}
     </AuthContext.Provider>
   );
