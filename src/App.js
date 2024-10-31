225367bc5845cd36ba351b0452cea57d30d0b78d
deivy-ramirez <davydwindsor@gmail.com>
2024-10-31 18:19:24 -0500

    creacion del panel admin2.0

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { useAuth } from './context/AuthContext';
 import Login from './components/Login';
 import Register from './components/Register';
 import Game from './components/Game';
