864e41d4d1b03263e1237656f0e45e4e47b2f619
deivy-ramirez <davydwindsor@gmail.com>
2024-11-05 19:24:52 -0500

    un front bonito

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './App';
+import 'index.css';
 import { AuthProvider } from './context/AuthContext';
 
 ReactDOM.render(
